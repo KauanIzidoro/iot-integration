7fa65b00d8165667cb883b28213b8b345e90e55b
KauanIzidoro <cnttbussineskauan@gmail.com>
2025-02-04 01:37:34 -0300

    :books: task1: update request-device.sh file to make temperature value is random

--- projects/task-1/request-device.sh
+++ projects/task-1/request-device.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 while true; do
+    TEMP=$(awk -v min=20 -v max=30 'BEGIN{srand(); print min+rand()*(max-min)}')
     curl -v -X POST http://localhost:8080/api/v1/DEMO_DEVICE_TK/telemetry \
         --header "Content-Type: application/json" \
-        --data '{"temperature": 25}'
+        --data "{\"temperature\": $TEMP}"
     sleep 1
 done
